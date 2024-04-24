a29c5abd3184c8ba68c637120464d5b630a31b70
Domenico Pagano <dpaganoh@gmail.com>
2024-04-23 23:09:38 -0500

    rutas + componentes

---
+++ src/views/Servicios/Servicios.jsx
@@ -0,0 +1,11 @@
+import style from './Servicios.module.css';
+
+const Servicios = () => {
+    return(
+        <div className={style.container}>
+            This is SERVICIOS
+        </div>
+    )
+}
+
+export default Servicios;
