f9ab9ade078a2e65e16a70fd32a75de87ea26443
MoniMcI <mbguantay@yahoo.com.ar>
2024-04-24 11:35:30 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
-import Nav from './components/nav/Nav.jsx';
+import Nav from './components/Nav/Nav.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home/Home.jsx';
+import Contacto from './views/Contacto/Contacto.jsx';
+import Nosotros from './views/Nosotros/Nosotros.jsx';
+import Servicios from './views/Servicios/Servicios.jsx';
 
 function App() {
-
 
   return (
     <div className='container'>
       <Nav />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/nosotros' element={<Nosotros />} />
+        <Route path='/servicios' element={<Servicios />} />
+        <Route path='/contacto' element={<Contacto />} />
+      </Routes>
     </div>
   )
 }
